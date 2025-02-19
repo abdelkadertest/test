02c23cfe78de3547e770d806b22e4ffbf21f94bb
Abdo <abdelkaderamria678@gmail.com>
2025-02-19 20:58:57 +0200

    dsa

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
+
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
